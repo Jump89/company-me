f95a4c72a597f8b9b4a4efaac10f367617a5d8c4
Marcio R <Murcielago203@gmail.com>
2022-02-15 18:18:43 -0500

    setup connection . js to connedct , set up db and schema

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS department;
+
+DROP TABLE IF EXISTS roles;
+
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE Table roles(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER
+);
+
+CREATE Table employee(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    maager_id INTEGER DEFAULT NULL
+);
+
+
