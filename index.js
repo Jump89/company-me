c60ed7606bff31c392d9f969ffa9d5f562212638
Marcio R <Murcielago203@gmail.com>
2022-02-15 21:43:51 -0500

    updated index.js with final add role , add employee , uodate employee functions, and end case .

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const choices = () => {
             type: 'list',
             name: 'options',
             message: 'What would you like to do?',
-            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role','End']
         }
     ).then(answer => {
         switch (answer.options) {
@@ -18,17 +18,20 @@ const choices = () => {
                 //do something
                 viewDepartment().then(answer => {
                     console.table(answer[0]);
+                    choices();
                 });
                 break;
             case 'view all roles':
                 viewRoles().then(answer => {
                     console.table(answer[0]);
+                    choices();
                 });
                 break;
 
             case 'view all employees':
                 viewEmployees().then(answer => {
                     console.table(answer[0]);
+                    choices();
                 });
                 break;
 
@@ -38,14 +41,14 @@ const choices = () => {
             case 'add a role':
                 addRole();
                 break;
-
             case 'add an employee':
                 addEmployee();
                 break;
-
             case 'update an employee role':
                 updateEmployee();
                 break;
+            case 'End': 
+                process.exit();
 
         }
     })
@@ -79,6 +82,7 @@ function addDepartment() {
                 console.log(`Added ${answers.department} to the database!`);
             }
         })
+        choices();
     })
 }
 
@@ -119,7 +123,97 @@ function addRole() {
                     console.log(`Added ${answers.title} to the database!`);
                 }
             })
+            choices();
     })
 }
+
+function addEmployee() {
+    let rolesArray = [];
+    viewRoles().then(roles => {
+        roles[0].forEach(role => {
+            let newObj = {
+                id: role.id,
+                name: role.title
+            }
+            rolesArray.push(newObj)
+        })
+
+    });
+    return inquirer.prompt([{
+        type: 'input',
+        name: "first_name",
+        message: 'Please enter First Name: '
+    }, {
+        type: 'input',
+        name: "last_name",
+        message: 'Please enter Last Name: '
+    }, {
+        type: 'list',
+        name: "role",
+        message: 'Please select your Role: ',
+        choices: rolesArray
+    }]).then(answers => {
+        let roleID = rolesArray.filter(x => answers.role === x.name)[0].id;
+        let result = db.query('INSERT INTO employee (first_name,last_name,role_id, manager_id) VALUES (?,?,?,?)', [answers.first_name, answers.last_name, roleID, null],
+            (err, results) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(`Added ${answers.first_name} ${answers.last_name} to the database!`);
+                }
+            })
+            choices();
+    })
+}
+
+function updateEmployee() {
+    let rolesArray = [];
+    let employeeArray = [];
+    viewRoles().then(roles => {
+        roles[0].forEach(role => {
+            let newObj = {
+                id: role.id,
+                name: role.title
+            }
+            rolesArray.push(newObj)
+        })
+    });
+    viewEmployees().then(results => {
+        results[0].forEach(result => {
+            let newObj = {
+                id: result.id,
+                name: result.first_name + " " + result.last_name
+            }
+            employeeArray.push(newObj)
+        })
+    }).then(() => {
+        return inquirer.prompt([{
+            type: 'list',
+            name: "employee",
+            message: 'Please select an Employee to update:',
+            choices: employeeArray
+        }, {
+            type: 'list',
+            name: "role",
+            message: 'Please update Employee Role:',
+            choices: rolesArray
+        }]).then(answers => {
+            let employeeID = employeeArray.filter(y => answers.employee === y.name)[0].id;
+
+            let roleID = rolesArray.filter(z => answers.role === z.name)[0].id;
+
+            let result = db.query('UPDATE employee SET role_id = (?) WHERE id = (?)', [roleID, employeeID], (err, results) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(`Updated ${answers.employee} to the database!`);
+                }
+            })
+            choices();
+        })
+    })
+}
+
+
 
 choices();
